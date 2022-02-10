8e96d4d8c58bd2c12c0929e231726f5030ee4d88
Alejandro M. Pi Cano <AlejandroPiCano@users.noreply.github.com>
2022-02-10 20:54:34 +0100

    productlistcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<div><h1>{{pageTitle}}</h1>My First Component</div>',
+  template: '<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>',
 
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
 }
+
